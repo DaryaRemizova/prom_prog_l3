e69a4e2851f5b1339d3f48f5e1d285684cbc6ff6
DaryaRemizova <urelifp@gmail.com>
2016-03-29 21:07:53 +0300

    get и set в классе Mood

--- src/var16/Mood.java
+++ src/var16/Mood.java
@@ -14,4 +14,12 @@ public class Mood {
         this.title = title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
 }
